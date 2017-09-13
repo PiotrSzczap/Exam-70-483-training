595c89ca769566c84eed26fadafaeff7aa492d25
Piotr Szczap <piotrszczap93@gmail.com>
2017-09-13 19:28:50 +0200

    Created solution and first project to objective 1.1.

---
+++ src/ImplementMultithreading/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementMultithreading
+{
+    class Program
+    {
+        /// <summary>
+        /// Objective 1.1
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
