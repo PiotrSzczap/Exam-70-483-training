2e8d2ea5d9d7e6d076a9e2abac31ea5881d97b24
Piotr Szczap <piotrszczap93@gmail.com>
2017-09-17 21:49:11 +0200

    Implemented synchronizing resources and async await sample.

--- ManageMultithreading/SynchronizingResources.cs
+++ ManageMultithreading/SynchronizingResources.cs
@@ -2,11 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ManageMultithreading
 {
-    class SynchronizingResources
+    static class SynchronizingResources
     {
+        public static object _locker = new object();
+
+        public static void LockSample()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                lock (_locker)
+                {
+                    Thread.Sleep(1000);
+                    //Do something
+                }
+            });
+            Thread.Sleep(100);
+            Task.Factory.StartNew(() =>
+            {
+                lock (_locker)
+                {
+                    //Do something
+                }
+            });
+        }
+
+        public static void MonitorSample()
+        {
+            object _gate = new object();
+            bool _lockToken = false;
+            Monitor.Enter(_lockToken, ref _lockToken);
+            Monitor.Exit(_gate);
+
+        }
+
+        public static void InterlockedSample()
+        {
+            int i = 0;
+            Interlocked.Add(ref i, 5);
+            int y = Interlocked.Exchange(ref i, 5);
+            int k=0;
+            Interlocked.CompareExchange(ref y, i, k);
+        }
+
     }
 }
--- src/ImplementMultithreading/AsyncAwaitSample.cs
+++ src/ImplementMultithreading/AsyncAwaitSample.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace ImplementMultithreading
 {
-    class AsyncAwaitSample
+    public static class AsyncAwaitSample
     {
+        public static async Task MethodAsync()
+        {
+            int i = await MethodWithResultAsync();
+            Console.WriteLine(i);
+        }
+
+        public static async Task<int> MethodWithResultAsync()
+        {
+            return await Task.FromResult(5);
+        }
     }
 }
