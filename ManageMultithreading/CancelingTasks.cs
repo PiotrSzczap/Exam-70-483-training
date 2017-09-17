7f258677abf3f6bfa7d0799dfb687ef3fbce4c19
Piotr Szczap <piotrszczap93@gmail.com>
2017-09-17 22:07:30 +0200

    Added cancelation token sample.

--- ManageMultithreading/CancelingTasks.cs
+++ ManageMultithreading/CancelingTasks.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ManageMultithreading
 {
-    class CancelingTasks
+    public static class CancelingTasks
     {
+        public static void CancelationTokenSample()
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+            Task task = Task.Run(() =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    // do something
+                }
+            });
+
+            cancellationTokenSource.Cancel();
+        }
     }
 }
