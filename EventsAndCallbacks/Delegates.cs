5f9f2cf18a288d6b752f0fb241d6b0f968fd81b1
Piotr Szczap <piotrszczap93@gmail.com>
2017-11-02 18:13:38 +0100

    Added lambdas and delegates.

---
+++ EventsAndCallbacks/Delegates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndCallbacks
+{
+    public class Delegates
+    {
+        public delegate int Calculate(int x, int y);
+        public int Add(int x, int y) { return x + y; }
+        public int Multiply(int x, int y) { return x * y; }
+        public void UseDelegate()
+        {
+            Calculate calc = Add;
+            Console.WriteLine(calc(3, 4)); 
+            calc = Multiply;
+            Console.WriteLine(calc(3, 4));
+            calc += Add;
+        }
+
+
+    }
+}
---
+++ EventsAndCallbacks/Lambdas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndCallbacks
+{
+    public class Lambdas
+    {
+        public delegate int Calculate(int x, int y);
+        public void Test()
+        {
+
+            Calculate meth = (x, y) =>
+            {
+                return x + y;
+            };
+            meth.Invoke(2,2);
+
+            Action action = () => { Console.WriteLine("Action"); };
+            Func<bool> func = () => { return true; };
+            Predicate<int> predicate = (i) => { return true; };
+        }
+    }
+}
