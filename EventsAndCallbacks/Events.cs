8d4e52edc46903f8a7e7d6e80696847d8a053902
Piotr Szczap <piotrszczap93@gmail.com>
2017-12-23 20:38:25 +0100

    Added exception handling and events.

---
+++ EventsAndCallbacks/Events.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndCallbacks
+{
+    class Events
+    {
+        public void CreateAndRaise()
+        {
+            Pub p = new Pub();
+            p.OnChange += (sender, e)
+            => Console.WriteLine("Event raised: { 0}", e.Value);
+            p.Raise();
+        }
+    }
+
+    public class Pub
+    {
+        private event EventHandler<MyArgs> onChange = delegate { };
+        public event EventHandler<MyArgs> OnChange
+        {
+            add
+            {
+                lock (onChange)
+                {
+                    onChange += value;
+                }
+            }
+            remove
+            {
+                lock (onChange)
+                {
+                    onChange -= value;
+                }
+            }
+        }
+        public void Raise()
+        {
+            onChange(this, new MyArgs(42));
+        }
+    }
+
+    public class Pub2
+    {
+        public event EventHandler OnChange = delegate { };
+        public void Raise()
+        {
+            var exceptions = new List<Exception>();
+            foreach (Delegate handler in OnChange.GetInvocationList())
+            {
+                try
+                {
+                    handler.DynamicInvoke(this, EventArgs.Empty);
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+            if (exceptions.Any())
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+    public class MyArgs : EventArgs
+    {
+        public MyArgs(int value)
+        {
+            Value = value;
+        }
+        public int Value { get; set; }
+    }
+}
---
+++ ImplementExceptionHandling/OrderProcessingException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementExceptionHandling
+{
+    [Serializable]
+    public class OrderProcessingException : Exception, ISerializable
+    {
+        public OrderProcessingException(int orderId)
+        {
+            OrderId = orderId;
+            this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+}
+        public OrderProcessingException(int orderId, string message)
+        : base(message)
+        {
+            OrderId = orderId;
+            this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+}
+        public OrderProcessingException(int orderId, string message,
+        Exception innerException)
+        : base(message, innerException)
+        {
+            OrderId = orderId;
+            this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+}
+        protected OrderProcessingException(SerializationInfo info, StreamingContext context)
+        {
+            OrderId = (int)info.GetValue("OrderId", typeof(int));
+        }
+        public int OrderId { get; private set; }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("OrderId", OrderId, typeof(int));
+        }
+    }
+}
