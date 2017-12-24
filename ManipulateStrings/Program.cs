c708f3c73720f3a6e42416ece50c7651873ae3cf
Piotr Szczap <piotrszczap93@gmail.com>
2017-12-24 13:45:49 +0100

    Added object life cycle and manipulating string.

---
+++ ManipulateStrings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ManipulateStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void StringBuilderTest()
+        {
+            StringBuilder sb = new StringBuilder(string.Empty);
+            for (int i = 0; i < 10000; i++)
+            {
+                sb.Append("x");
+            }
+        }
+
+        static void StringWriterTest()
+        {
+            var stringWriter = new StringWriter();
+            using (XmlWriter writer = XmlWriter.Create(stringWriter))
+            {
+                writer.WriteStartElement("book");
+                writer.WriteElementString("price", "19.95");
+                writer.WriteEndElement();
+                writer.Flush();
+            }
+            string xml = stringWriter.ToString();
+        }
+    }
+}
---
+++ ObjectLifeCycle/Unmanaged.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifeCycle
+{
+    class UnmangedWrapper : IDisposable
+    {
+        public FileStream Stream { get; private set; }
+        public UnmangedWrapper()
+        {
+            this.Stream = File.Open("temp.dat", FileMode.Create);
+        }
+        ~UnmangedWrapper()
+        {
+            Dispose(false);
+        }
+        public void Close()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            System.GC.SuppressFinalize(this);
+        }
+        public void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (Stream != null)
+                {
+                    Stream.Close();
+                }
+            }
+        }
+    }
+}
