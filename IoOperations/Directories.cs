a39bacf0460b2f96d2f508097b7fd8f31cfab8c1
Piotr Szczap <piotrszczap93@gmail.com>
2018-03-11 19:39:07 +0100

    Added io operations.

---
+++ IoOperations/Directories.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoOperations
+{
+    class Directories
+    {
+        public void Execute()
+        {
+            if (Directory.Exists(@"C:\Temp\ProgrammingInCSharp\Directory"))
+            {
+                Directory.Delete(@"C:\Temp\ProgrammingInCSharp\Directory");
+            }
+            var directoryInfo = new DirectoryInfo(@"C:\Temp\ProgrammingInCSharp\DirectoryInfo");
+            if (directoryInfo.Exists)
+            {
+                directoryInfo.Delete();
+            }
+
+
+            directoryInfo.Create();
+            DirectorySecurity directorySecurity = directoryInfo.GetAccessControl();
+            directorySecurity.AddAccessRule(
+             new FileSystemAccessRule("everyone",
+             FileSystemRights.ReadAndExecute,
+             AccessControlType.Allow));
+            directoryInfo.SetAccessControl(directorySecurity);
+        }
+
+        private static void ListDirectories(DirectoryInfo directoryInfo,string searchPattern, int maxLevel, int currentLevel)
+        {
+            if (currentLevel >= maxLevel)
+            {
+                return;
+            }
+            string indent = new string('-', currentLevel);
+            try
+            {
+                DirectoryInfo[] subDirectories = directoryInfo.GetDirectories(searchPattern);
+                foreach (DirectoryInfo subDirectory in subDirectories)
+                {
+                    Console.WriteLine(indent + subDirectory.Name);
+                    ListDirectories(subDirectory, searchPattern, maxLevel, currentLevel + 1);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // You don’t have access to this folder. 
+                Console.WriteLine(indent +"Can’taccess:" +directoryInfo.Name);
+                return;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                // The folder is removed while iterating
+                Console.WriteLine(indent +"Can’tfind:" +directoryInfo.Name);
+                return;
+            }
+        }
+    }
+}
---
+++ IoOperations/Drives.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoOperations
+{
+    class Drives
+    {
+        public void Execute()
+        {
+            DriveInfo[] drivesInfo = DriveInfo.GetDrives();
+            foreach (DriveInfo driveInfo in drivesInfo)
+            {
+                Console.WriteLine("Drive{ 0}", driveInfo.Name);
+                Console.WriteLine("  Filetype:{ 0}", driveInfo.DriveType);
+                if (driveInfo.IsReady == true)
+                {
+                    Console.WriteLine("  Volumelabel:{ 0}", driveInfo.VolumeLabel);
+                    Console.WriteLine("  Filesystem:{ 0}", driveInfo.DriveFormat);
+                    Console.WriteLine(
+            "  Availablespacetocurrentuser:{ 0, 15} bytes",
+            driveInfo.AvailableFreeSpace);
+                    Console.WriteLine(
+            "  Totalavailablespace:{ 0, 15} bytes",
+            driveInfo.TotalFreeSpace);
+                    Console.WriteLine(
+            "  Totalsizeofdrive:{ 0, 15}  bytes",
+            driveInfo.TotalSize);
+                }
+            }
+        }
+    }
+}
