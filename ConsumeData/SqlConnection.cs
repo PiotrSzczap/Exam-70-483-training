57a8bf36871312823cab45edf6eed400bb89ad9d
Piotr Szczap <piotrszczap93@gmail.com>
2018-05-06 18:49:18 +0200

    Added connection to database.

---
+++ ConsumeData/SqlConnection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ConsumeData
+{
+    public class SqlConnectionBuilder
+    {
+        public void ConnectionStringBuilder()
+        {
+            var sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            sqlConnectionStringBuilder.DataSource = @"(localdb)\v11.0";
+            sqlConnectionStringBuilder.InitialCatalog = "ProgrammingInCSharp";
+            string connectionString = sqlConnectionStringBuilder.ToString();
+        }
+
+        public async Task SelectDataFromTable()
+        {
+            string connectionString = "";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("UPDATE People SET FirstName =’John’",
+                connection);
+                await connection.OpenAsync();
+                int numberOfUpdatedRows = await command.ExecuteNonQueryAsync();
+                Console.WriteLine("Updated { 0}rows", numberOfUpdatedRows);
+            }
+        }
+
+        public void Transaction()
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                using (SqlConnection connection = new SqlConnection(""))
+                {
+                    connection.Open();
+                    SqlCommand command1 = new SqlCommand("INSERT INTO People([FirstName], [LastName], [MiddleInitial]) VALUES('John', ‘Doe’, null)",connection);
+                    SqlCommand command2 = new SqlCommand("INSERT INTO People([FirstName], [LastName], [MiddleInitial]) VALUES(‘Jane’, ‘Doe’, null)",connection);
+                    command1.ExecuteNonQuery();
+                    command2.ExecuteNonQuery();
+                }
+                transactionScope.Complete();
+            }
+        }
+    }
+
+}
