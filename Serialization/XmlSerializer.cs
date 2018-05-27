abeed3b5163f670aad92a048f1d01c19715f5c37
Piotr Szczap <piotrszczap93@gmail.com>
2018-05-27 14:40:35 +0200

    Added serialization.

---
+++ Serialization/XmlSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    class XmlSerializator
+    {
+        public class Data { }
+        public void Execute()
+        {
+            string s = "";
+            using (StringWriter sw = new StringWriter())
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Data));
+                xmlSerializer.Serialize(sw, new Data());
+                s = sw.ToString();
+            }
+        }
+    }
+}
